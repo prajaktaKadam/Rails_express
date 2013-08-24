398c6251f27c055a97fe6b1539403e2b6ce5b5d7
prajaktakadam <prjkta2011@gmail.com>
2013-08-24 15:25:00 +0530

    Adding Blogging website skeleton, with user resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsExpress::Application.config.secret_key_base = '0824b85d438a6ae046c22e249d633f37c989aada820beba7979cbd44543f28a746a1d5afb22cc8104d8482dea4898962c6373a0ff16a40a4af151f7eda706814'
---
+++ db/migrate/20130824070755_add_dob_to_users.rb
@@ -0,0 +1,6 @@
+class AddDobToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :dob, :datetime
+  		
+  end
+end
